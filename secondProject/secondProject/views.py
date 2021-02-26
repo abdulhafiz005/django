de10a28b87531684cf8a182b3e7b7fae78343902
abdulhafiz005 <creativedraftsman@gmail.com>
2021-02-26 12:56:11 +0500

    second task

--- secondProject/secondProject/views.py
+++ secondProject/secondProject/views.py
@@ -5,5 +5,18 @@ def values(request):
     return render(request, 'values.html')
 
 def sum(request):
-    sum = {"sum": (int(request.GET.get('val1')) + int(request.GET.get('val2')))}
-    return render(request, 'sum.html', sum)
+    ans = None
+    operator = request.GET.get('operator')
+    val1 = request.GET.get('val1')
+    val2 = request.GET.get('val2')
+    if operator == '+':
+        ans = {"ans": (int(val1) + int(val2))}
+    elif operator == '-':
+        ans = {"ans": (int(val1) - int(val2))}
+    elif operator == '*':
+        ans = {"ans": (int(val1) * int(val2))}
+    elif operator == '/':
+        ans = {"ans": (int(val1) / int(val2))}
+    else:
+        ans = {"ans": "something went wrong."}
+    return render(request, 'sum.html', ans)
